078abf51ebcf00847f0c3b167707c125f88a2545
Omot147 <omotomoniyi@gmail.com>
2025-12-06 07:08:57 +0100

    js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 module.exports = {
     theme: {
       extend: {
-        backgroundImage: {
-          'dark-gradient-top': 'linear-gradient(to top, rgba(0, 0, 0, 0.7), transparent)',
-          'dark-gradient-left': 'linear-gradient(to left, rgba(0, 0, 0, 0.7), transparent)',
-        }
-      }
-    }
+
+      },
+    },
+    plugins []
+
+    variants:{}
   }
